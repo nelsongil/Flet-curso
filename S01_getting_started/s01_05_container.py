53bb42b6ba0d5da9f1787426fba89038103759c1
nelsongil <info@nelsongil.com>
2023-11-06 22:08:56 +0100

    Update s01_05_container.py

--- S01_getting_started/s01_05_container.py
+++ S01_getting_started/s01_05_container.py
@@ -1,12 +1,24 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.add(
+    lenguajes = ['Python', 'Flet', 'Flutter']
+    etiquetas = []
+    
+    for e in lenguajes:
+        etiquetas.append(ft.Text(e))
+        
+    row_datos = ft.Row(controls=etiquetas)
+    
+    page.add(row_datos)
+    
+    
+    # Este era el contenido del main, formas distintas de crear controles
+    """page.add(
         ft.Row(controls=[
             ft.Text("Python"),
             ft.Text("Flet"),
             ft.Text("Row")
         ])
-    )
+    )"""
 
 ft.app(target=main)

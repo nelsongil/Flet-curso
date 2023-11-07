285c3994440d3bf65e3c697cc2bf556557c2ca28
nelsongil <info@nelsongil.com>
2023-11-07 20:04:31 +0100

    Update s01_06_saludo.py

--- S01_getting_started/s01_06_saludo.py
+++ S01_getting_started/s01_06_saludo.py
@@ -4,19 +4,20 @@ def main(page: ft.Page):
     page.window_width = 500        # window's width is 200 px
     page.window_height = 400       # window's height is 200 px
     page.window_resizable = False  # window is not resizable
+    txtnombre=ft.TextField(label='Digite su nombe')
+
 
     def saludar(e):
         if not txtnombre.value:
             txtnombre.error_text = "Please enter your name"
-            page.update()
+            txtnombre.update()
         else:
             name = txtnombre.value
             page.add(ft.Text(f"Hello, {name}!")) 
     
-    txtnombre=ft.TextField
     
     row = ft.Row(controls=[
-        txtnombre(label='Digite su nombe'),
+        txtnombre,
         ft.ElevatedButton(text='Saludar', on_click=saludar)
     ])
 

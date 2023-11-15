c74e31c44da0d2dcc6892cbda8af4c0dcdb08772
nelsongil <info@nelsongil.com>
2023-11-15 21:00:45 +0100

    Create demo_01_drag_drop_hole receive.py

---
+++ S06_Drag&Drop/demo_01_drag_drop_hole receive.py
@@ -0,0 +1,69 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Ejemplo de Drag and Drop"
+    
+    def drag_accept(e):
+        # get draggable (source) control by its ID
+        src = page.get_control(e.src_id)
+        # update text inside draggable control
+        src.content.content.value = "0"
+        # update text inside drag target control
+        e.control.content.content.value = "1"
+        # reset border
+        e.control.content.border = None
+        page.update()
+        
+    def drag_will_accept(e):
+        # black border when it's allowed to drop and red when it's not
+        e.control.content.border = ft.border.all(
+            2, ft.colors.BLACK45 if e.data == "true" else ft.colors.RED
+        )
+        e.control.update()
+
+    def drag_leave(e):
+        e.control.content.border = None
+        e.control.update()
+        
+    page.add(
+        ft.Row(
+            [
+                ft.Draggable(
+                    group="number",
+                    content=ft.Container(
+                        width=50,
+                        height=50,
+                        bgcolor=ft.colors.CYAN_200,
+                        border_radius=5,
+                        content=ft.Text("1", size=20),
+                        alignment=ft.alignment.center, 
+                    ),
+                    content_when_dragging=ft.Container(
+                        width=50,
+                        height=50,
+                        bgcolor=ft.colors.BLUE_GREY_200,
+                        border_radius=5,
+                    ),
+                    content_feedback=ft.Text("1"),              
+                ),
+                ft.Container(width=100),
+                ft.DragTarget(
+                    group="number",
+                    content=ft.Container(
+                        width=50,
+                        height=50,
+                        bgcolor=ft.colors.DEEP_PURPLE_400,
+                        border_radius=5,
+                        content=ft.Text("0", size=20),
+                        alignment=ft.alignment.center,
+                    ),
+                    on_accept=drag_accept,
+                    #añado para el borde asigno las funciones creadas
+                    on_will_accept=drag_will_accept,
+                    on_leave=drag_leave,
+                ),
+            ]
+        )
+    )
+
+ft.app(target=main)

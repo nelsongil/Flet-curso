84373166899b9cea921ff3eae2aa735d656b06c4
nelsongil <info@nelsongil.com>
2023-11-15 20:22:40 +0100

    Create demo_05_listview_batches.py

---
+++ S045_ large_lists/demo_05_listview_batches.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+def main(page:ft.Page):
+     lvw_textos = ft.ListView(expand=1, spacing=10, item_extent=50)
+     page.add(lvw_textos)
+     
+     for i in range(5100):
+         lvw_textos.controls.append(ft.Text(f"Line {i}"))
+         
+         if i % 500 == 0:
+             page.update()
+    
+ft.app(target=main, view=ft.W)

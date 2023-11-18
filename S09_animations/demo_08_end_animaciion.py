579cd5e58ccb4aee48b1a4fb7559b0a67c75bc13
nelsongil <info@nelsongil.com>
2023-11-18 19:47:24 +0100

    Create demo_08_end_animaciion.py

---
+++ S09_animations/demo_08_end_animaciion.py
@@ -0,0 +1,42 @@
+import flet as ft 
+
+# En este ejmplo que es igual a la demo_01, se añade una función que imprime un texto al finalizar la animación
+
+def main(page: ft.Page):
+    contenedor = ft.Container(
+        alignment=ft.alignment.center,
+        gradient=ft.LinearGradient(
+            begin=ft.alignment.top_left,
+            end=ft.Alignment(0.1, 1),
+            colors=[
+                "0xff1f005c",
+                "0xff5b0060",
+                "0xff870160",
+                "0xffac255e",
+                "0xffca485c",
+                "0xffe16b5c",
+                "0xfff39060",
+                "0xffffb56b",
+            ],
+            tile_mode=ft.GradientTileMode.MIRROR,
+        ),
+        width=150,
+        height=150,
+        bgcolor="blue",
+        border_radius=10,
+        animate_opacity=300,
+        on_animation_end=lambda e: print("Contenedor animado terminado:", e.data)
+    )
+    
+    def animate_opacity(e):
+        contenedor.opacity = 0 if contenedor.opacity == 1 else 1
+        contenedor.update()
+        
+    page.add(
+        contenedor,ft.ElevatedButton(
+            "Animar opacidad",
+            on_click=animate_opacity,
+            ),
+    )
+    
+ft.app(target=main)

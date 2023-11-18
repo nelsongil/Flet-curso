cbee2728283a5025cdb1091c297ab020ec60a94e
nelsongil <info@nelsongil.com>
2023-11-18 19:36:45 +0100

    Create demo_07_switch_contenido_animado.py

---
+++ S09_animations/demo_07_switch_contenido_animado.py
@@ -0,0 +1,29 @@
+import time
+
+import flet as ft
+
+def main(page: ft.Page):
+
+    i = ft.Image(src="https://picsum.photos/150/150", width=150, height=150)
+
+    def animate(e):
+        sw.content = ft.Image(
+            src=f"https://picsum.photos/150/150?{time.time()}", width=150, height=150
+        )
+        page.update()
+
+    sw = ft.AnimatedSwitcher(
+        i,
+        transition=ft.AnimatedSwitcherTransition.SCALE,
+        duration=500,
+        reverse_duration=500,
+        switch_in_curve=ft.AnimationCurve.BOUNCE_OUT,
+        switch_out_curve=ft.AnimationCurve.BOUNCE_IN,
+    )
+
+    page.add(
+        sw,
+        ft.ElevatedButton("Animate!", on_click=animate),
+    )
+
+ft.app(target=main)
